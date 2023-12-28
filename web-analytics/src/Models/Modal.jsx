ee5ca27080544d24a2b7f1a9b9aee9a2a0b2e4dc
Anupama Ravindran <anupama.r@techversantinfotech.com>
2023-12-28 10:03:32 +0530

    modified DataTable.jsx,App.css

--- web-analytics/src/Models/Modal.jsx
+++ web-analytics/src/Models/Modal.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+// import React from 'react';
 
-const Modal = ({ handleClose, show, dataOutput }) => {
-  const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+// const Modal = ({ handleClose, show, dataOutput }) => {
+//   const showHideClassName = show ? 'modal display-block' : 'modal display-none';
 
-  return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        <h2>{dataOutput}</h2>
-        <button onClick={handleClose}>Close</button>
-      </section>
-    </div>
-  );
-};
+//   return (
+//     <div className={showHideClassName}>
+//       <section className="modal-main">
+//         <h2>{dataOutput}</h2>
+//         <button onClick={handleClose}>Close</button>
+//       </section>
+//     </div>
+//   );
+// };
 
-export default Modal;
+// export default Modal;
